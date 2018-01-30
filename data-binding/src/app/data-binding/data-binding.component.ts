96822752c030c4199253e46838f43a944c756038
Sávio Bispo Maciel <savio.maciel@cnpq.br>
2018-01-30 17:37:46 -0200

    Event binding.

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -13,21 +13,42 @@ export class DataBindingComponent implements OnInit {
 
   urlImagem: String = 'http://lorempixel.com/400/200/nature/';
 
+  valorAtual: String;
+
+  valorSalvo: String;
+
+  isMouseOver: Boolean;
+
   constructor() { }
 
   ngOnInit() {
   }
 
   getValor(): Number {
-
     return 15 * 5;
-
   }
 
   getCurtirCurso(): Boolean {
-
     return true;
+  }
+  
+  salvar(): void {
+    alert("salvando...");
+  }
 
+  salvarValor(valorSalvo: String): void {
+    this.valorSalvo = valorSalvo;
+  }
+
+  onMouseOverOut(): void {
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+  onKeyUp(evento: KeyboardEvent): void {
+    console.log("Tecla clicada: " + evento.key + "\n");
+    console.log("Código da tecla: " + evento.code);
+    console.log("Id: " + (<HTMLInputElement>evento.target).id);
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
   }
 
 }
