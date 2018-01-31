a248296c779585bcb47b9523920232f3622f3a5e
Sávio Bispo Maciel <savio.maciel@cnpq.br>
2018-01-31 16:35:42 -0200

    angular 4 rest com spring boot.

---
+++ angular-cli-spring-boot-example/user-app/src/app/user/user.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { User } from "./user";
+import { Http, Response } from "@angular/http";
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch';
+import { Observable } from "rxjs/Observable";
+
+@Injectable()
+export class UserService {
+
+  private apiUrl = 'http://localhost:8080/users';
+
+  constructor(private http: Http) {
+  }
+
+  findById(id: number): Observable<User> {
+    return this.http.get(this.apiUrl + '/' + id)
+      .map((res:Response) => res.json())
+      .catch((error:any) => Observable.throw(error.json().error || 'Error'));
+  }
+
+  saveUser(user: User): Observable<User> {
+
+    return this.http.post(this.apiUrl, user)
+      .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+
+  }
+
+  deleteUserById(id: number): Observable<boolean> {
+    return this.http.delete(this.apiUrl + '/' + id)
+      .map((res:Response) => res.json())
+      .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+  updateUser(user: User): Observable<User> {
+    return this.http.put(this.apiUrl + "/find", user)
+      .map((res:Response) => res.json())
+      .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+
+  }
+
+  findAll(): Observable<User[]>  {
+    return this.http.get(this.apiUrl)
+      .map((res:Response) => res.json())
+      .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+}
